d3fa928b1976e6df7ac49857503d868f7ac252c8
CHICKENSHOP <155237912+CHICKENSHOP@users.noreply.github.com>
2024-01-10 21:41:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
-var exportDetails = "Restaurant: " + selectedRestaurant +
+function calculateTotalBill() {
+    var selectedRestaurant = document.getElementById('restaurantSelect').value;
+
+    var bonelessPrice = parseFloat(document.getElementById('bonelessPrice').value);
+    var bodyPrice = parseFloat(document.getElementById('bodyPrice').value);
+    var lollipopPrice = parseFloat(document.getElementById('lollipopPrice').value);
+    var fryPrice = parseFloat(document.getElementById('fryPrice').value);
+    var jointPrice = parseFloat(document.getElementById('jointPrice').value);
+    var khawabPrice = parseFloat(document.getElementById('khawabPrice').value);
+    var wingsPrice = parseFloat(document.getElementById('wingsPrice').value);
+    var dumPrice = parseFloat(document.getElementById('dumPrice').value);
+
+    var bonelessQty = parseInt(document.getElementById('bonelessQty').value);
+    var bodyQty = parseInt(document.getElementById('bodyQty').value);
+    var lollipopQty = parseInt(document.getElementById('lollipopQty').value);
+    var fryQty = parseInt(document.getElementById('fryQty').value);
+    var jointQty = parseInt(document.getElementById('jointQty').value);
+    var khawabQty = parseInt(document.getElementById('khawabQty').value);
+    var wingsQty = parseInt(document.getElementById('wingsQty').value);
+    var dumQty = parseInt(document.getElementById('dumQty').value);
+
+    var previousBalance = parseFloat(document.getElementById('previousBalance').value);
+
+    var bonelessTotal = bonelessPrice * bonelessQty;
+    var bodyTotal = bodyPrice * bodyQty;
+    var lollipopTotal = lollipopPrice * lollipopQty;
+    var fryTotal = fryPrice * fryQty;
+    var jointTotal = jointPrice * jointQty;
+    var khawabTotal = khawabPrice * khawabQty;
+    var wingsTotal = wingsPrice * wingsQty;
+    var dumTotal = dumPrice * dumQty;
+
+    var totalBill = bonelessTotal + bodyTotal + lollipopTotal + fryTotal +
+                    jointTotal + khawabTotal + wingsTotal + dumTotal;
+
+    var totalWithPreviousBalance = totalBill + previousBalance;
+
+    var resultText = "Restaurant: " + selectedRestaurant +
+        "<br>Previous Balance: $" + previousBalance.toFixed(2) +
+        "<br>Current Bill: $" + totalBill.toFixed(2) +
+        "<br>Total Amount Due: $" + totalWithPreviousBalance.toFixed(2);
+
+    document.getElementById('result').innerHTML = resultText;
+
+    var exportDetails = "Restaurant: " + selectedRestaurant +
         "\nBoneless Quantity: " + bonelessQty +
         "\nBody Quantity: " + bodyQty +
         "\nLollipop Quantity: " + lollipopQty +
         "\nFry Quantity: " + fryQty +
-        "\nJoint Quantity: " + jointQty +
-        "\nKhawab Quantity: " + khawabQty +
-        "\nWings Quantity: " + wingsQty +
-        "\nDum Quantity: " + dumQty +
-        "\nTotal Bill: $" + totalWithPreviousBalance.toFixed(2);
-
-    document.getElementById('exportDetails').value = exportDetails;
-}
-
-function exportToPDF() {
-    var element = document.getElementById('result');
-    var pdfName = document.getElementById('restaurantSelect').value + "_" + new Date().toLocaleDateString() + ".pdf";
-    html2pdf(element, { filename: pdfName });
-}
-
-function exportToImage() {
-    var element = document.getElementById('result');
-    domtoimage.toBlob(element)
-        .then(function (blob) {
-            var link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = document.getElementById('restaurantSelect').value + "_" + new Date().toLocaleDateString() + ".png";
-            link.click();
-        });
-}
+        "\nJoint Quantity: " +
