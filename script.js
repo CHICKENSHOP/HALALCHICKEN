fde723ef7c570030bb71613920551921fec5e75b
CHICKENSHOP <155237912+CHICKENSHOP@users.noreply.github.com>
2024-01-10 22:19:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,41 @@
 function calculateTotalBill() {
     var selectedRestaurant = document.getElementById('restaurantSelect').value;
 
-    var bonelessPrice = parseFloat(document.getElementById('bonelessPrice').value);
-    var bodyPrice = parseFloat(document.getElementById('bodyPrice').value);
-    var lollipopPrice = parseFloat(document.getElementById('lollipopPrice').value);
-    var fryPrice = parseFloat(document.getElementById('fryPrice').value);
-    var jointPrice = parseFloat(document.getElementById('jointPrice').value);
-    var khawabPrice = parseFloat(document.getElementById('khawabPrice').value);
-    var wingsPrice = parseFloat(document.getElementById('wingsPrice').value);
-    var dumPrice = parseFloat(document.getElementById('dumPrice').value);
-
-    var bonelessQty = parseInt(document.getElementById('bonelessQty').value);
-    var bodyQty = parseInt(document.getElementById('bodyQty').value);
-    var lollipopQty = parseInt(document.getElementById('lollipopQty').value);
-    var fryQty = parseInt(document.getElementById('fryQty').value);
-    var jointQty = parseInt(document.getElementById('jointQty').value);
-    var khawabQty = parseInt(document.getElementById('khawabQty').value);
-    var wingsQty = parseInt(document.getElementById('wingsQty').value);
-    var dumQty = parseInt(document.getElementById('dumQty').value);
+    var weightOfChicken = parseFloat(document.getElementById('weightOfChicken').value);
+    var quantityOfChicken = parseInt(document.getElementById('quantityOfChicken').value);
+    var pricePerKg = parseFloat(document.getElementById('pricePerKg').value);
 
     var previousBalance = parseFloat(document.getElementById('previousBalance').value);
 
-    var bonelessTotal = bonelessPrice * bonelessQty;
-    var bodyTotal = bodyPrice * bodyQty;
-    var lollipopTotal = lollipopPrice * lollipopQty;
-    var fryTotal = fryPrice * fryQty;
-    var jointTotal = jointPrice * jointQty;
-    var khawabTotal = khawabPrice * khawabQty;
-    var wingsTotal = wingsPrice * wingsQty;
-    var dumTotal = dumPrice * dumQty;
+    // Calculate total for Kori restaurant
+    if (selectedRestaurant === 'koriBakeway') {
+        var totalWeightCost = weightOfChicken * pricePerKg;
+        var totalQuantityCost = quantityOfChicken * 10;  // Assuming a fixed cost per chicken quantity
 
-    var totalBill = bonelessTotal + bodyTotal + lollipopTotal + fryTotal +
-                    jointTotal + khawabTotal + wingsTotal + dumTotal;
+        var totalBill = totalWeightCost + totalQuantityCost;
+        var totalWithPreviousBalance = totalBill + previousBalance;
 
-    var totalWithPreviousBalance = totalBill + previousBalance;
+        var resultText = "Restaurant: " + selectedRestaurant +
+            "<br>Previous Balance: $" + previousBalance.toFixed(2) +
+            "<br>Weight Cost: $" + totalWeightCost.toFixed(2) +
+            "<br>Quantity Cost: $" + totalQuantityCost.toFixed(2) +
+            "<br>Total Bill: $" + totalBill.toFixed(2) +
+            "<br>Total Amount Due: $" + totalWithPreviousBalance.toFixed(2);
 
-    var resultText = "Restaurant: " + selectedRestaurant +
-        "<br>Previous Balance: $" + previousBalance.toFixed(2) +
-        "<br>Current Bill: $" + totalBill.toFixed(2) +
-        "<br>Total Amount Due: $" + totalWithPreviousBalance.toFixed(2);
+        document.getElementById('result').innerHTML = resultText;
 
-    document.getElementById('result').innerHTML = resultText;
+        var exportDetails = "Restaurant: " + selectedRestaurant +
+            "\nWeight of Chicken: " + weightOfChicken + " kg" +
+            "\nQuantity of Chicken: " + quantityOfChicken +
+            "\nPrice per Kg: $" + pricePerKg.toFixed(2) +
+            "\nTotal Weight Cost: $" + totalWeightCost.toFixed(2) +
+            "\nTotal Quantity Cost: $" + totalQuantityCost.toFixed(2) +
+            "\nTotal Bill: $" + totalBill.toFixed(2);
+    } else {
+        // Logic for other restaurants (similar to the existing code)
+        // ...
+    }
 
-    var exportDetails = "Restaurant: " + selectedRestaurant +
-        "\nBoneless Quantity: " + bonelessQty +
-        "\nBody Quantity: " + bodyQty +
-        "\nLollipop Quantity: " + lollipopQty +
-        "\nFry Quantity: " + fryQty +
-        "\nJoint Quantity: " +
+    // Set exportDetails for PDF export
+    document.getElementById('exportDetails').value = exportDetails;
+}
