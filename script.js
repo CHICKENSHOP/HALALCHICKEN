0454f95ed26e13072691010a04dc6bded4a854b9
CHICKENSHOP <155237912+CHICKENSHOP@users.noreply.github.com>
2024-01-10 22:28:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,20 @@ function calculateTotalBill() {
     // Set exportDetails for PDF export
     document.getElementById('exportDetails').value = exportDetails;
 }
+
+function exportToPDF() {
+    var element = document.getElementById('result');
+    var pdfName = document.getElementById('restaurantSelect').value + "_" + new Date().toLocaleDateString() + ".pdf";
+    html2pdf(element, { filename: pdfName });
+}
+
+function exportToImage() {
+    var element = document.getElementById('result');
+    domtoimage.toBlob(element)
+        .then(function (blob) {
+            var link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = document.getElementById('restaurantSelect').value + "_" + new Date().toLocaleDateString() + ".png";
+            link.click();
+        });
+}
