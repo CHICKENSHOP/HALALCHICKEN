5a033d4ae6d6a54fe55cf301d2ac3b05e49754ab
CHICKENSHOP <155237912+CHICKENSHOP@users.noreply.github.com>
2024-01-11 21:46:25 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+// Replace with your Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA6tSdZQ6d8-5hgeTpYEwvNbW2cqC5Z0aQ",
+    authDomain: "mubarak-chicken.firebaseapp.com",
+    projectId: "mubarak-chicken",
+    storageBucket: "mubarak-chicken.appspot.com",
+    messagingSenderId: "160226866721",
+    appId: "1:160226866721:web:eb9ff0193554e21739d560"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference to the Firestore database
+const db = firebase.firestore();
+
+// Populate the dropdown with restaurant names
+const restaurantDropdown = document.getElementById('restaurantDropdown');
+
+db.collection('restaurants').get()
+    .then((snapshot) => {
+        snapshot.forEach((doc) => {
+            const option = document.createElement('option');
+            option.value = doc.data().pageURL;
+            option.text = doc.data().name;
+            option.lang = "en"; // Set language attribute to English (default)
+            restaurantDropdown.add(option);
+        });
+    })
+    .catch((error) => {
+        console.error('Error getting restaurants: ', error);
+    });
+
+// Function to load the selected restaurant's page
+function loadRestaurantPage() {
+    const selectedPageURL = restaurantDropdown.value;
+    if (selectedPageURL) {
+        window.location.href = selectedPageURL;
+    }
+}
